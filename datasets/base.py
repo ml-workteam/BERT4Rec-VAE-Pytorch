a1040a753e9bc16dc479c272a0ab52d267dc3e7b
Tot Nekto <32616057+ml-workteam@users.noreply.github.com>
2023-03-30 13:44:36 +0300

    Update base.py
    start smap from 1

--- datasets/base.py
+++ datasets/base.py
@@ -131,7 +131,7 @@ class AbstractDataset(metaclass=ABCMeta):
     def densify_index(self, df):
         print('Densifying index')
         umap = {u: i for i, u in enumerate(set(df['uid']))}
-        smap = {s: i for i, s in enumerate(set(df['sid']))}
+        smap = {s: i+1 for i, s in enumerate(set(df['sid']))}
         df['uid'] = df['uid'].map(umap)
         df['sid'] = df['sid'].map(smap)
         return df, umap, smap
